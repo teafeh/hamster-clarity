a618e93245a26c55995ae2ff52ffe1045605f806
Teafeh24 <31405163+teafeh@users.noreply.github.com>
2026-07-14 07:43:47 +0100

    Uncomment routes and related imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from '@/contexts/AuthContext'
-import AppRouter from './routes/AppRouter'
+// import AppRouter from './routes/AppRouter'
 
-// import ComingSoon from '@/pages/ComingSoon'
-// import NotFoundPage from '@/pages/WaitlistNotFoundPage'
-// import BetaOptInPage from './pages/BetaOptInPage'
+import ComingSoon from '@/pages/ComingSoon'
+import NotFoundPage from '@/pages/WaitlistNotFoundPage'
+import BetaOptInPage from './pages/BetaOptInPage'
 
 export default function App() {
   return (
     <AuthProvider>
-      <AppRouter />
-      {/* <Routes>
+      {/* <AppRouter /> */}
+       <Routes>
         <Route path="/" element={<ComingSoon />} />
         <Route path="*" element={<NotFoundPage />} />
         <Route path="/beta" element={<BetaOptInPage />} />
-      </Routes> */}
+      </Routes>
     </AuthProvider>
   )
 }
